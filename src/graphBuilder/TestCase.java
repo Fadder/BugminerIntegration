a7922f6a99c95c732cecad79fc6e74c4160bfa38
Fadder <vincentbecker@gmx.de>
2017-01-16 15:02:47 +0100

    add TestCaseID // TODO don't count it, name it

--- src/graphBuilder/TestCase.java
+++ src/graphBuilder/TestCase.java
@@ -6,15 +6,26 @@ import executor.monitoring.Edge;
 
 public class TestCase {
 	
-	//TODO ID
+	//TODO name ID
+	static int instanceCounter = 0;
+	private int id;
 	private HashMap<String,MethodGraph> methodGraphs = new HashMap<>();
     private boolean isSuccessful;
+    
+    public TestCase(){
+    	this.id = instanceCounter;
+    	instanceCounter++;
+    }
     
     public void update(Edge edge){
     	MethodGraph methodGraph = methodGraphs.computeIfAbsent(edge.getMethod(), k -> new MethodGraph(edge.getMethod()));
         methodGraph.update(edge);
     }
     
+	public int getId() {
+		return id;
+	}
+
 	public boolean isSuccessful() {
 		return isSuccessful;
 	}
