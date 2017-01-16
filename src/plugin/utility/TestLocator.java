7a6c433c98b3cd8ca57b23f08fc40eed7616cca8
dinhhle <ledinhhu@hu-berlin.de>
2017-01-16 18:41:52 +0100

    test klassen alphabetisch sortiert

--- src/plugin/utility/TestLocator.java
+++ src/plugin/utility/TestLocator.java
@@ -1,13 +1,15 @@
 package plugin.utility;
 
 import java.lang.reflect.MalformedParametersException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.IAnnotation;
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IImportDeclaration;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IPackageFragment;
@@ -16,18 +18,15 @@ import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 
-
 public class TestLocator {
-	
+
 	/**
-	 * Findet alle Testklassen fï¿½r das Projekt
+	 * Findet alle Testklassen für das Projekt
+	 * 
 	 * @param project das Projekt
 	 * @return alle Testklassen mit vollqualifizierten Namen
 	 */
-	public static List<String> findTestClasses(IProject project){
-		List<String> fullyQualifiedTestClasses = new ArrayList<>();
-		IJavaProject javaProject=null;
-		// Wir untersuchen nur java Projekte
+	public static Collection<String> findTestClasses(IProject project) { 
 		try {
 			if (!project.getDescription().hasNature(JavaCore.NATURE_ID)) {
 				throw new MalformedParametersException();
@@ -35,44 +34,64 @@ public class TestLocator {
 		} catch (CoreException e) {
 			e.printStackTrace();
 		}
-		
-		javaProject=JavaCore.create(project);
-		
-		
-		boolean found=false;
-		
-		
-		// Findet ï¿½ber die Eclipse API alle Klassen, die eine Methode haben, die mit @Test annotiert ist
-		// TODO das hier evtl auflï¿½sen bzw einen besseren Weg finden die Testklassen sicher zu differenzieren??
+
+		IJavaProject javaProject = JavaCore.create(project);
+		Set<String> collectedTestClasses = new TreeSet<>();
+
 		try {
-			for(IPackageFragment fragment:javaProject.getPackageFragments()){
-				if(fragment.getKind()==IPackageFragmentRoot.K_SOURCE){
-					//gehe durch alle Package Fragmente, die source dateien enthalten
-					for(ICompilationUnit unit:fragment.getCompilationUnits()){
-						for(IType type: unit.getAllTypes()){
-							// durch alle Klassen
-							for(IMethod method: type.getMethods()){
-								// durch alle Methoden
-								for(IAnnotation annotation: method.getAnnotations()){
-									// testen, ob eine @Test Annotation besteht
-									if(annotation.getElementName().equals("Test")){
-										found=true;
-										fullyQualifiedTestClasses.add(type.getFullyQualifiedName());
-									}
-									if(found){break;}
-								}
-								if(found){break;}
+			for (IPackageFragment fragment : javaProject.getPackageFragments()) {
+				if (fragment.getKind() == IPackageFragmentRoot.K_SOURCE) {
+					for (ICompilationUnit unit : fragment.getCompilationUnits()) {
+						if(containsJUnitImport(unit)){
+							String testClass=getTestClass(unit);
+							if(unit!=null){
+								collectedTestClasses.add(testClass);
 							}
-							if(found){break;}
 						}
-						if(found){	found=false;}
 					}
 				}
 			}
 		} catch (JavaModelException e) {
 			e.printStackTrace();
 		}
-		
-		return fullyQualifiedTestClasses;
+
+		return collectedTestClasses;
+	}
+
+	/**
+	 * Prüft, ob der JUnit Test in die Klasse importiert wurde
+	 */
+	private static boolean containsJUnitImport(ICompilationUnit unit) throws JavaModelException {
+		for (IImportDeclaration imp : unit.getImports()) {
+			if (imp.getElementName().equals("org.junit.Test")) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Gibt den Namen der Klasse zurück,
+	 * wenn sie einen Test beinhaltet.
+	 * 
+	 * @return vollqualfizierter Namen der Klasse, die einen Test beinhaltet, null wenn kein Test vorhanden
+	 */
+	private static String getTestClass(ICompilationUnit unit)
+			throws JavaModelException {
+
+		for (IType type : unit.getAllTypes()) {
+			for (IMethod method : type.getMethods()) {
+				for (IAnnotation annotation : method.getAnnotations()) {
+
+					String annotationName= annotation.getElementName();
+					if (annotationName.equals("Test")||annotationName.equals("org.junit.Test")){
+						return type.getFullyQualifiedName();
+					}
+
+				}
+			}
+
+		}
+		return null;
 	}
 }
