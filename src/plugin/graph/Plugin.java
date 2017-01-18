0847587866c506fdb27ad8cafb86e7a98a4c009d
Fadder <vincentbecker@gmx.de>
2017-01-18 11:36:02 +0100

    comment out unused window object in Plugin

--- src/plugin/graph/Plugin.java
+++ src/plugin/graph/Plugin.java
@@ -6,22 +6,18 @@ import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.jface.dialogs.MessageDialog;
-
-
-
 
 /**
  * Using Eclispe's Hello World template.
  * @see IWorkbenchWindowActionDelegate
  */
 public class Plugin implements IWorkbenchWindowActionDelegate {
-	private IWorkbenchWindow window;
+	// private IWorkbenchWindow window;
+	
 	/**
 	 * The constructor.
 	 */
-	public Plugin() {
-	}
+	public Plugin() { }
 
 	/**
 	 * The action has been activated. The argument of the
@@ -60,7 +56,8 @@ public class Plugin implements IWorkbenchWindowActionDelegate {
 	 * be able to provide parent shell for the message dialog.
 	 * @see IWorkbenchWindowActionDelegate#init
 	 */
+	@Override
 	public void init(IWorkbenchWindow window) {
-		this.window = window;
+		//this.window = window;
 	}
 }
