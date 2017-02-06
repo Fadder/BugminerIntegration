2d892333785a5a3565b3e23e2f2e79ce72fa55c3
berweilj <janberw.eiler@gmail.com>
2017-02-06 20:51:19 +0100

    minor changes for testing

--- src/plugin/gui/Testing.java
+++ src/plugin/gui/Testing.java
@@ -1,10 +1,12 @@
 package plugin.gui;
 
+import java.awt.Component;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
-import com.sun.codemodel.internal.JLabel;
 
 import plugin.graphviz.GraphViz;
 
@@ -14,20 +16,30 @@ public class Testing {
 		// TODO Auto-generated method stub
 		
 		GraphViz gv = new GraphViz();
-		System.out.println(gv.getTempDir());
-		System.out.println(gv.getExecutable());
+		//StartTabbedPane stp = new StartTabbedPane();
+		System.out.println(StartTabbedPane.getOsname());
+		System.out.println(StartTabbedPane.getTempDir());
 		//gv.setTempDir("hello");
 		//gv.setExecutable("hello");
 		
+		
+		
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
-		//JLabel temDirLabel = new JLabel("GraphViz Temp-Dir:");
-		JTextField tempDirField = new JTextField(gv.getTempDir());
-		JTextField exeField = new JTextField(gv.getExecutable());
+		panel.setLayout(new GridLayout(3, 1));
+		// 3 rows, 1 column Panel having Grid Layout
+				
+		//JLabel tempDirLabel = new JLabel("text");
+		JLabel label = new JLabel("This is a basic label");
+		JTextField tempDirField = new JTextField(StartTabbedPane.getTempDir());
+		JTextField exeField = new JTextField(StartTabbedPane.getExecutable());
 		
+		panel.add(label);
 		panel.add(tempDirField);
 		panel.add(exeField);
 		frame.add(panel, 0);
+		
+		frame.pack();
 		frame.setVisible(true);
 	}
 
